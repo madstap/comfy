c5f1d98414a5425ca796ee50ec13a173668d7cba
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-06-27 18:05:02 -0300

    Release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject madstap/comfy "0.1.1"
+(defproject madstap/comfy "0.1.2"
   :description "Clojure(script) utils"
   :url "http://example.com/madstap/comfy"
   :license {:name "Eclipse Public License"
