d0971cc97b566dda8365add4ccb460f6d4793cfd
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-07-20 17:40:15 -0300

    Add walk to the cljs test-runner

--- test/madstap/comfy/test_runner.cljs
+++ test/madstap/comfy/test_runner.cljs
@@ -1,6 +1,7 @@
 (ns madstap.comfy.test-runner
   (:require
    [doo.runner :refer-macros [doo-tests]]
-   [madstap.comfy.core-test]))
+   [madstap.comfy.core-test]
+   [madstap.comfy.walk-test]))
 
-(doo-tests 'madstap.comfy.core-test)
+(doo-tests 'madstap.comfy.core-test 'madstap.comfy.walk-test)
