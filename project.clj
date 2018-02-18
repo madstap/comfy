4db56bb99d792b171a0b8316bbdba003fa84b8c6
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2018-02-17 23:40:32 -0300

    Bump clj version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]]
 
   :plugins [[lein-codox "0.10.3"]
